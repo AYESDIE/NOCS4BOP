f34767d656df615344e515c402115201f369908a
AYESDIE <34449856+AYESDIE@users.noreply.github.com>
2024-11-28 01:01:29 +0100

    FIX: fix for unknown file type.

--- scripts/process_models.py
+++ scripts/process_models.py
@@ -12,9 +12,18 @@ for input_dir in ["models", "models_eval"]:
         continue
     os.makedirs(f"processed_{input_dir}", exist_ok=True)
     for file_name in os.listdir(input_dir):
-        file_path = os.path.join(input_dir, file_name)
-        file_name = file_name.split(".")[0]
-        point_cloud = open3d.io.read_point_cloud(file_path)
+        file_name = os.path.join(input_dir, file_name)
+        if file_name.endswith(".ply"):
+            point_cloud = open3d.io.read_point_cloud(file_name)
+            points = numpy.asarray(point_cloud.points)
+        elif file_name.endswith(".obj"):
+            point_cloud = open3d.io.read_triangle_mesh(file_name)
+            points = numpy.asarray(point_cloud.vertices)
+        else:
+            print(f"Unknown file type: {file_name}")
+            continue
+        
+        file_name_wo_ext = file_name.split(".")[0]
         points = numpy.asarray(point_cloud.points)
         nocs_points, shift, scale = calculate_nocs(points)
-        numpy.savez(os.path.join(f"processed_{input_dir}", f"{file_name}_nocs.npz"), nocs_points=nocs_points, shift=shift, scale=scale)
+        numpy.savez(os.path.join(f"processed_{input_dir}", f"{file_name_wo_ext}_nocs.npz"), nocs_points=nocs_points, shift=shift, scale=scale)
